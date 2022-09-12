3580a03ba9c8d97e150fa0b2524b0e741df3e4dc
Júlio <juliocesar.jc585@gmail.com>
2022-09-12 14:59:10 -0300

    Encapsulamento
    Realizado encapsulamento dos dados do programa e realizado a inserção do decorator @property

--- Projeto/Cliente.py
+++ Projeto/Cliente.py
@@ -1,7 +1,17 @@
 class Cliente:
     def __init__(self, n, fone):
-        self.nome = n
-        self.telefone = fone
+        self._nome = n
+        self._telefone = fone
         #parametros n e fone são criados para inicialização dos atributos
 
     #comando pass= nenhuma estrutura foi definida ainda
+
+    #Método GET - acessar atributos que estão privados
+
+    def get_nome(self):
+        return self._nome
+
+    #Método SET
+
+    def set_nome(self, nome):
+        self._nome= nome
--- Projeto/Conta.py
+++ Projeto/Conta.py
@@ -3,3 +3,15 @@ class Conta:
         self.saldo=0
         self.numero= numero
         self.titular=titular
+
+        @property
+        def get_saldo(self):
+            return self._saldo
+
+
+        @saldo.setter
+        def set_saldo(self, saldo):
+            if saldo<0:
+                print("\033[1;31mO SALDO INFORMADO NÃO PODE SER NEGATIVO\033[m")
+            else:
+                self._saldo= saldo
