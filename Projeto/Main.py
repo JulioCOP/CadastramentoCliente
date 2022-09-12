05380f62ff021172833b7fbe27c2ab3f91208078
Júlio <juliocesar.jc585@gmail.com>
2022-09-12 18:19:59 -0300

    Modulo Menu
    Criado novo modulo para o titulo do menu

--- Projeto/Main.py
+++ Projeto/Main.py
@@ -4,6 +4,7 @@ print("Testando o projeto")
 
 from Cliente import Cliente
 from Conta import Conta
+from Menu import Menu
 #referência de classe
 
 primeiro_cliente= Cliente(" ")
@@ -11,6 +12,6 @@ primeiro_cliente= Cliente(" ")
 #Cliente(strings) = nome da classe instanciada + atributos do metodo construtor
 
 
-print(primeiro_cliente._nome, " e ", "Nº do CPF:",primeiro_cliente._cpf)
 conta= Conta(primeiro_cliente._nome, numero=" ")
-print(conta._titular, "Numero da conta:", conta._numero, "Saldo R$:",f"{conta._saldo:.2f}")
+Menu(" DADOS DO CLIENTE ")
+print("CLIENTE: ",conta._titular, "\nNumero da conta:", conta._numero, "\nSaldo R$:",f"{conta._saldo:.2f}")
---
+++ Projeto/Menu.py
@@ -0,0 +1,5 @@
+def Menu(msg):
+    tam=len(msg)
+    print("~"*tam)
+    print(f'{msg:^15}')
+    print("~"*tam)
